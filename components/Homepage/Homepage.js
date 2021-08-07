8ad7a88cb7902bb35b4266ece3abff9abefdca86
fairyinabottle4 <keithlow98@gmail.com>
2021-08-07 11:37:05 +0800

    Improve design of homepage

--- components/Homepage/Homepage.js
+++ components/Homepage/Homepage.js
@@ -19,11 +19,23 @@ const Homepage = (props) => {
   return (
     <div>
       <h2>Where would you like to go today?</h2>
-      <p>You have liked {likedPlaces.length} / 1144 places</p>
-      <p>You have visited {visitedPlaces.length} / 1144 places</p>
+      <div style={likedStatsContainer}>
+        <p style={likedStats}>You have liked {likedPlaces.length} / 1144 places</p>
+        {likedPlaces.length === 0 ? <p>There's something for everyone here!</p> : 
+            likedPlaces.length < 10 ? <p> Have you planned your next trip?</p>
+            : <p>Woah explorer! Take it easy!</p>}
+      </div>
+      <div style={likedStatsContainer}>
+        <p style={likedStats}>You have visited {visitedPlaces.length} / 1144 places</p>
+        {visitedPlaces.length === 0 ? <p>A great journey begins with a single step!</p>
+            : visitedPlaces.length < 500 ? <p>Making good progress, traveller!</p> 
+            : <p>You could write a book now!</p>}
+      </div>
       <div style={homeContainer}>
         <div style={likeContainer}>
-          <h2>Places I like</h2>
+          <div>
+            <h2 style={likedPlacesTitle}>Places I like</h2>
+          </div>
           {likedPlaces?.map(place => <LikedItem id={place.id} likedPlace={place} user={currUser}/>)}
         </div>
         <div>
@@ -44,8 +56,26 @@ const homeContainer = {
 
 const likeContainer = {
   border: '2px solid',
+  backgroundColor: '#ffc4ff'
 }
 
 const visitContainer = {
   border: '2px solid red',
+}
+
+const likedPlacesTitle = {
+  backgroundColor: '#ce93d8',
+  textAlign: 'center'
+}
+
+const likedStatsContainer = {
+  display: 'flex'
+}
+
+const likedStats = {
+  marginRight: '10em'
+}
+
+const likedStatsComments = {
+
 }
